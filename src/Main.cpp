858541fb29f0eaa9cfc058985926da1c5c12a3fe
Grady O'Connell <flipcoder@gmail.com>
2016-02-08 14:03:03 -0800

    its happening...

---
+++ src/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <memory>
+#include <thread>
+#include "Qor/kit/kit.h"
+#include "Qor/Qor.h"
+#include "Info.h"
+#include "GameState.h"
+
+#include "Qor/kit/log/log.h"
+#include "Qor/kit/async/async.h"
+
+#ifdef DEBUG
+    #include <backward/backward.cpp>
+#endif
+
+using namespace std;
+using namespace kit;
+
+int main(int argc, const char** argv)
+{
+    
+    Args args(argc, argv);
+    args.set("mod","FRAG.EXE");
+    //args.set("basic_shader","fog");
+    //args.set("no_loading_fade","true");
+    
+    Texture::DEFAULT_FLAGS = Texture::TRANSPARENT | Texture::MIPMAP;
+    
+#ifndef DEBUG
+    try{
+#endif
+        auto engine = kit::make_unique<Qor>(args);
+        engine->states().register_class<GameState>("game");
+        engine->run("game");
+#ifndef DEBUG
+    }catch(const Error&){
+        // already logged
+    }catch(const std::exception& e){
+        LOGf("Uncaught exception: %s", e.what());
+    }
+#endif
+    return 0;
+}
+
